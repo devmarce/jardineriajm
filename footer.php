3aaa36f18c68ed525759a4dea377808a8b89b367
marcelo <98444716+devmarce@users.noreply.github.com>
2024-09-01 14:41:42 -0300

    init

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <div style="background-color: black; display: flex;">
+        <div class="p-1">
+            <img src="./img/logosvg/trebol.svg" alt="" style="width: 100px;">
+        </div>
+        <div class="p-1" style="width: 90%; margin-top: 6px;">
+            <p style="color: #1efa079e;text-align: center;">&copy; JARDINERIA JM - <?php echo date('Y') ?> &#127808;</p>
+        </div>
+      </div>
+</footer>
