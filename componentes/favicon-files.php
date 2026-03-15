27940ec027a2c98f1941cf6db02db27faa6b5f91
marcelo <98444716+devmarce@users.noreply.github.com>
2026-03-15 20:37:55 -0300

    add sitemap

--- componentes/favicon-files.php
+++ componentes/favicon-files.php
@@ -3,7 +3,6 @@
 <link rel="apple-touch-icon" sizes="72x72" href="/img/favicon/apple-icon-72x72.png">
 <link rel="apple-touch-icon" sizes="76x76" href="/img/favicon/apple-icon-76x76.png">
 <link rel="apple-touch-icon" sizes="120x120" href="/img/favicon/apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/img/favicon/apple-icon-144x144.png">
 <link rel="apple-touch-icon" sizes="152x152" href="/img/favicon/apple-icon-152x152.png">
 <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-icon-180x180.png">
 <link rel="icon" type="image/png" sizes="192x192" href="/img/favicon/android-icon-192x192.png">
